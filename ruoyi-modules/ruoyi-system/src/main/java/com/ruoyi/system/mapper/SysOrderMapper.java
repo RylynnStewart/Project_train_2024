f95fd6b53536457f7134457a9674386e264b2fdc
HuChangtao <99883338+HuChangtao@users.noreply.github.com>
2024-07-13 10:37:41 +0800

    Add files via upload

--- ruoyi-modules/ruoyi-system/src/main/java/com/ruoyi/system/mapper/SysOrderMapper.java
+++ ruoyi-modules/ruoyi-system/src/main/java/com/ruoyi/system/mapper/SysOrderMapper.java
@@ -1,12 +1,11 @@
 package com.ruoyi.system.mapper;
-
 import java.util.List;
 import com.ruoyi.system.domain.SysOrder;
 
 /**
  * 交易订单Mapper接口
- *
- * @author ryan
+ * 
+ * @author HuChangtao
  * @date 2024-07-12
  */
 public interface SysOrderMapper
@@ -21,7 +20,7 @@ public interface SysOrderMapper
 
     /**
      * 查询交易订单列表
-     *
+     * 
      * @param sysOrder 交易订单
      * @return 交易订单集合
      */
@@ -29,7 +28,7 @@ public interface SysOrderMapper
 
     /**
      * 新增交易订单
-     *
+     * 
      * @param sysOrder 交易订单
      * @return 结果
      */
@@ -37,7 +36,7 @@ public interface SysOrderMapper
 
     /**
      * 修改交易订单
-     *
+     * 
      * @param sysOrder 交易订单
      * @return 结果
      */
@@ -45,7 +44,7 @@ public interface SysOrderMapper
 
     /**
      * 删除交易订单
-     *
+     * 
      * @param id 交易订单主键
      * @return 结果
      */
@@ -53,14 +52,14 @@ public interface SysOrderMapper
 
     /**
      * 批量删除交易订单
-     *
+     * 
      * @param ids 需要删除的数据主键集合
      * @return 结果
      */
     public int deleteSysOrderByIds(Long[] ids);
 
     /**
-     * 查交易订单的日期与交易额
+     * 查询交易订单的日期与交易额
      */
     public List<SysOrder> selectEc(SysOrder sysOrder);
 }
